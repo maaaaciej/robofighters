6f5b7098c82dc38fd9a473d9d3163b3dba0cb368
Maciej <ostromaciej@gmail.com>
2020-06-29 12:47:24 +0200

    Updates

---
+++ precache-manifest.c1ef784414359a548eef9dd29099dbec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25beea37dfb3784b1e1f525e47f1112",
+    "url": "/robofighters/index.html"
+  },
+  {
+    "revision": "01e2b7e003b2451c8767",
+    "url": "/robofighters/static/css/main.e5266d9a.chunk.css"
+  },
+  {
+    "revision": "2095e00630b99174d515",
+    "url": "/robofighters/static/js/2.a3c5ed19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofighters/static/js/2.a3c5ed19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e2b7e003b2451c8767",
+    "url": "/robofighters/static/js/main.32a93662.chunk.js"
+  },
+  {
+    "revision": "5ef9f18e4dd8633dab74",
+    "url": "/robofighters/static/js/runtime-main.d3115fed.js"
+  }
+]);
